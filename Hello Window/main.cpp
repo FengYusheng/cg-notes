4649a907ca8380ffeb936d6d1d41d95b92e55c51
Feng, Yusheng <Yusheng.Feng@clarivate.com>
2023-05-03 12:58:27 +0800

    Create a window with glfw.

--- Hello Window/main.cpp
+++ Hello Window/main.cpp
@@ -18,6 +18,11 @@ int main()
     std::cout << "GLFW library compile information: " << glfwGetVersionString() << std::endl;
     std::cout << "GLFW version information: " << glfw_major << "." << glfw_minor << "." << glfw_rev << std::endl;
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+
     glfwTerminate();
     return 0;
 }
