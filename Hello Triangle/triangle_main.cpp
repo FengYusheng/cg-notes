a0dcdbec4ed92c290d31aff03754ea4d9053574f
Feng, Yusheng <Yusheng.Feng@clarivate.com>
2023-07-05 19:56:53 +0800

    Hello Triangle

--- Hello Triangle/triangle_main.cpp
+++ Hello Triangle/triangle_main.cpp
@@ -1,7 +1,27 @@
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 #include <iostream>
 
 int main()
 {
-    std::cout << "Wang Yan" << std::endl;
-    return 0
+    int glfw_major = 0;
+    int glfw_minor = 0;
+    int glfw_rev = 0;
+
+    /*
+     * Initialization hints
+     * https://www.glfw.org/docs/latest/intro_guide.html#init_hints
+     * The values you set hints to are never reset by GLFW, but they only take effect during initialization.
+     * Once GLFW has been initialized, any values you set will be ignored until the library is terminated and initialized again.
+     */
+    glfwInitHint(GLFW_JOYSTICK_HAT_BUTTONS, GLFW_FALSE);
+    glfwInit();
+    glfwGetVersion(&glfw_major, &glfw_minor, &glfw_rev);
+
+    std::cout << "glfw version: " << glfw_major << "." << glfw_minor << "." << glfw_rev <<std::endl;
+    std::cout << glfwGetVersionString() << std::endl;
+
+    glfwTerminate();
+    return 0;
 }
