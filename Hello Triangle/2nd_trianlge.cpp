a4119ed7e56351bc84018d78c46514905af52cda
Yusheng Feng <Yusheng.Feng@clarivate.com>
2023-09-30 17:16:11 +0800

    My 2nd triangle: load opengl api

--- Hello Triangle/2nd_trianlge.cpp
+++ Hello Triangle/2nd_trianlge.cpp
@@ -1,3 +1,7 @@
+/*
+ * https://www.glfw.org/docs/latest/context_guide.html#context_glext_auto
+ * By including the glad header before the GLFW header, it suppresses the development environment's OpenGL or OpenGL ES header.
+ */
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
@@ -14,6 +18,15 @@ int main()
     int glfw_major = 0;
     int glfw_minor = 0;
     int glfw_rev = 0;
+    GLint success = 0;
+    GLchar infoLog[512] = {0};
+
+    /* vertex positions */
+    float vertices[] = {
+            -0.5f, 0.0f, 0.0f,
+            0.5f, 0.0f, 0.0f,
+            0.0f, 0.5f, 0.0f
+    };
 
     /* compile-time version */
     std::cout << "GLFW compile-time version: " << GLFW_VERSION_MAJOR << "." << GLFW_VERSION_MINOR << "." << GLFW_VERSION_REVISION << std::endl;
@@ -49,9 +62,44 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
 #endif
 
-    /* Create shader objects and load shader source codes to shader objects */
+    /* Windows creation*/
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "My 2nd Triangle", NULL, NULL);
+    if (NULL == window)
+    {
+        std::cout << "Failed to create a glfw window." << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
-    /* Compile shaders */
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    /*
+     * Load OpenGL context function address.
+     * This step must be after the above 2 steps.
+     * And we must load opengl context function pointers before creating shader objects.
+     * https://www.glfw.org/docs/latest/context_guide.html#context_glext_auto
+     * Load opengl api from system space to user space.
+     */
+    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Create shader objects, load shader source codes to shader objects and compile shaders */
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION FAILED\n" << infoLog << std::endl;
+        glfwTerminate();
+        return -1;
+    }
 
     /* Create a program object and attach shader objets to that program object */
 
@@ -73,26 +121,7 @@ int main()
 
     /* Select a polygon rasterization mode */
 
-    /* Windows creation and  event processing */
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "My 2nd Triangle", NULL, NULL);
-    if (NULL == window)
-    {
-        std::cout << "Failed to create a glfw window." << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
-    /* This step must be below the above 2 steps. */
-    if(!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress))
-    {
-        std::cout << "Failed to initialize GLAD" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-
+    /* Event processing */
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
